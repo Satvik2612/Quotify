d3018b648ac45434a4291b5eb2a37bc0db6330a9
Satvik Gandhi <satvikagandhi@gmail.com>
2024-04-06 14:51:07 +0530

    initial commit

---
+++ src/test/java/com/booquo/Quotify/QuotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booquo.Quotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
